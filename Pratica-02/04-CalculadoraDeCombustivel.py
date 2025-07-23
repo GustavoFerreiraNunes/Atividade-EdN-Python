b1e88b27362a60dfeeb45d55225530bce75a2c88
Gustavo Ferreira Nunes <gustavo.ferreiranunes1@gmail.com>
2025-07-23 19:43:10 -0300

    Respondida as atividades do 1 ao 4 da prática 2

---
+++ Pratica-02/04-CalculadoraDeCombustivel.py
@@ -0,0 +1,10 @@
+distancia_km = 300
+combustivel_litros = 25
+
+consumo_medio = distancia_km / combustivel_litros
+
+print("4 - Calculadora de Consumo de Combustível")
+print(f"Distância percorrida: {distancia_km} km")
+print(f"Combustível gasto: {combustivel_litros} litros")
+print(f"Consumo médio: {consumo_medio:.2f} km/l")
+print("-" * 40)
